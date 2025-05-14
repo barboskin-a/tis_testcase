7fe4f1264ca1fef4dad45f41ad2bcb2024a211c1
barboskin-a <lizik.rabotyga45@gmail.com>
2025-05-14 11:54:18 +0700

    Модуль регистрация (позитивные и негативные сценарии)

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,45 +1,75 @@
 describe('Модуль Регистрация',() => {
     it('Модуль Регистрация', () => {
         cy.visit('https://dev.profteam.su/registration')
-        cy.get('input[autocomplete="username"]').type('shiero');
+        cy.get('input[autocomplete="username"]').type('shieroel');
         cy.get('input[type="email"]').type('liza.shirokikh05@mail.ru');
         cy.get('input[type="password"]').first().type('Password1');
         cy.get('input[type="password"]').eq(1).type('Password1');
         cy.get('div.registration-form__button').contains('Далее').should('be.visible')
-            .and('not.have.css', 'display', 'none').click()
+            .and('not.have.css', 'display', 'none').click();
+
         cy.get('input[autocomplete="family-name"]').type('Широких');
         cy.get('input[autocomplete="given-name"]').type('Елизавета');
         cy.get('input[autocomplete="additional-name"]').should('exist').and('not.have.attr', 'required');
         cy.get('input[autocomplete="additional-name"]').type('Романовна');
         cy.get('input[type="file"]').should('exist').and('not.have.attr', 'required');
         cy.get('input[type="file"]').selectFile('cypress/fixtures/avatar.jpg', { force: true });
+        cy.get('div.registration-form__button').contains('Создать аккаунт').should('be.visible')
+            .and('not.have.css', 'display', 'none').click();
+        cy.visit('https://dev.profteam.su/account/main');
 
     });
     it('Некорректная почта (негативный) Модуль регистрация', () => {
         cy.visit('https://dev.profteam.su/registration')
         cy.get('input[autocomplete="username"]').type('login')
-        cy.get('input[type="email"]').type('email-for-negative-test')
+        cy.get('input[type="email"]').type('email-for-negative-test');
         cy.get('input[type="password"]').first().type('Password1');
         cy.get('input[type="password"]').eq(1).type('Password1');
-        cy.get('div.registration-form__button').contains('Далее').click({force: true})
+        cy.get('div.registration-form__button').contains('Далее').click({force: true});
     });
 
-    it('Существующий логин (негативный) Модуль регистрация', () => {
+    it('Некорректный логин (негативный) Модуль регистрация', () => {
         cy.visit('https://dev.profteam.su/registration')
-        cy.get('input[autocomplete="username"]').type('testerStudent');
-        cy.get('input[type="email"]').type('liza.shirokikh05@mail.ru');
+        cy.get('input[autocomplete="username"]').type('testerStudent1');
+        cy.get('input[type="email"]').type('email2025@mail.ru');
         cy.get('input[type="password"]').first().type('Password1');
         cy.get('input[type="password"]').eq(1).type('Password1');
-        cy.get('div.registration-form__button').contains('Далее').click({force: true})
+        cy.get('div.registration-form__button').contains('Далее').click({force: true});
     });
 
     it('Несовпадение паролей (негативный) Модуль регистрация', () => {
         cy.visit('https://dev.profteam.su/registration')
         cy.get('input[autocomplete="username"]').type('login');
-        cy.get('input[type="email"]').type('liza.shirokikh05@mail.ru');
+        cy.get('input[type="email"]').type('email2025@mail.ru');
         cy.get('input[type="password"]').first().type('Password1');
-        cy.get('input[type="password"]').eq(1).type('Pasword2');
-        cy.get('div.registration-form__button').contains('Далее').click({force: true})
+        cy.get('input[type="password"]').eq(1).type('dge53');
+        cy.get('div.registration-form__button').contains('Далее').click({force: true});
     });
 
+    it('Слишком простой пароль (негативный) Модуль регистрация', () => {
+        cy.visit('https://dev.profteam.su/registration')
+        cy.get('input[autocomplete="username"]').type('login');
+        cy.get('input[type="email"]').type('email2025@mail.ru');
+        cy.get('input[type="password"]').first().type('123');
+        cy.get('input[type="password"]').eq(1).type('123');
+        cy.get('div.registration-form__button').contains('Далее').click({force: true})
+        cy.contains('Персональные данные').should('not.exist');
+    });
+
+    it('Латиница в полях ввода персональных данных (негативный) Модуль регистрация', () => {
+        cy.visit('https://dev.profteam.su/registration')
+        cy.get('input[autocomplete="username"]').type('login');
+        cy.get('input[type="email"]').type('email2025@mail.ru');
+        cy.get('input[type="password"]').first().type('Password1');
+        cy.get('input[type="password"]').eq(1).type('Password1');
+        cy.get('div.registration-form__button').contains('Далее').should('be.visible')
+            .and('not.have.css', 'display', 'none').click();
+
+        cy.get('input[autocomplete="family-name"]').type('Shirokikh');
+        cy.get('input[autocomplete="given-name"]').type('Elizaveta');
+        cy.get('input[autocomplete="additional-name"]').should('exist').and('not.have.attr', 'required');
+        cy.get('input[autocomplete="additional-name"]').type('Romanovna');
+        cy.get('input[type="file"]').should('exist').and('not.have.attr', 'required');
+        cy.get('input[type="file"]').selectFile('cypress/fixtures/avatar.jpg', { force: true });
+    });
 })
