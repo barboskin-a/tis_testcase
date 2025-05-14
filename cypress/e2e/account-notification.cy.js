73e6162e8c99c41521d17813da00e773e2c4ced3
barboskin-a <lizik.rabotyga45@gmail.com>
2025-05-15 00:54:42 +0700

    Account-notification

--- cypress/e2e/account-notification.cy.js
+++ cypress/e2e/account-notification.cy.js
@@ -1,12 +1,26 @@
 describe('Модуль Личный кабинет', () => {
-    it('Просмотр уведомлений Модуль личный кабинет', () =>{
+    it('Просмотр уведомлений Модуль личный кабинет', () => {
         cy.visit('https://dev.profteam.su/login')
         cy.get('input[autocomplete="username"]').type('shieroel');
         cy.get('input[type="password"]').type('Password1');
         cy.get('div.login-form__button').contains('Войти').should('be.visible')
             .and('not.have.css', 'display', 'none').click();
 
-        cy.visit('https://dev.profteam.su/account/main');
-        cy.get('div.notification-bell').click();
-    })
+        cy.url().should('include', '/account/main');
+        cy.get('div.header-container__desktop', { timeout: 10000 }).should('be.visible').click();
+        cy.visit('https://dev.profteam.su/notification')
+    });
+
+    it('Чтение уведомлений Модуль личный кабинет', () => {
+        cy.visit('https://dev.profteam.su/login')
+        cy.get('input[autocomplete="username"]').type('shieroel');
+        cy.get('input[type="password"]').type('Password1');
+        cy.get('div.login-form__button').contains('Войти').should('be.visible')
+            .and('not.have.css', 'display', 'none').click();
+
+        cy.url().should('include', '/account/main');
+        cy.get('div.header-container__desktop', { timeout: 10000 }).should('be.visible').click();
+        cy.visit('https://dev.profteam.su/notification');
+        cy.get('button[type=submit]').eq(2).click()
+    });
 })
